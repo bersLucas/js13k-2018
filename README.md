c91f4ff01966a05e2378be023c0e85b61c992417
Lucas Bersier <bers.lucas@gmail.com>
2018-08-15 23:19:45 -0400

    Use PointerLock API to move element

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # js13k-2018
 
 ## Blog posts
-* https://js13k.lucasbersier.com/mouse-movement-with-css-vars-in-806-bytes
+* [Mouse movement with CSS --vars in < 600 bytes](https://js13k.lucasbersier.com/mouse-movement-with-css-vars-in-806-bytes)
+* [Using the Pointer Lock API](https://js13k.lucasbersier.com/using-the-pointer-lock-api)
--- src/js/game.js
+++ src/js/game.js
@@ -1,14 +1,32 @@
 'use strict';
 
 var elem = document.getElementById('gamecube');
+var box = document.getElementById('box');
+var position = { x: 0, y: 0
 
-document.addEventListener('mousemove', function (e) {
-  requestAnimationFrame(function () {
-    return move(e);
-  });
+  // Initalize PointerLock API.
+};elem.addEventListener('click', function (e) {
+  elem.requestPointerLock();
 });
 
-function move(e) {
-  document.documentElement.style.setProperty('--mouse-x', e.clientX + 'px');
-  document.documentElement.style.setProperty('--mouse-y', e.clientY + 'px');
+document.addEventListener('pointerlockchange', lockChangeAlert, false);
+document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
+
+function lockChangeAlert() {
+  if (document.pointerLockElement === elem || document.mozPointerLockElement === elem) {
+    document.addEventListener('mousemove', updatePosition, false);
+  } else {
+    document.removeEventListener('mousemove', updatePosition, false);
+  }
+}
+
+function updatePosition(e) {
+  position.x += e.movementX;
+  position.y += e.movementY;
+
+  requestAnimationFrame(draw);
+}
+
+function draw() {
+  box.style.cssText = 'transform: translateX(' + position.x + 'px) translateY(' + position.y + 'px)';
 }
