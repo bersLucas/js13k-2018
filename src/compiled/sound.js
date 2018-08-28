6af611ae201561c91bae87fc2ce8952256d47db5
Lucas Bersier <bers.lucas@gmail.com>
2018-08-27 22:05:50 -0400

    Min/max frequency, lower volume

--- src/compiled/sound.js
+++ src/compiled/sound.js
@@ -1,22 +1,28 @@
 'use strict';
 
 var goodSound = function goodSound(keyCode) {
-  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  var oscillator = audioCtx.createOscillator();
-  // keyCodes 66 -> 60 and hZ 200 -> 800
-  oscillator.frequency.value = 25 * keyCode - 1650 + 200;
-  oscillator.connect(audioCtx.destination);
-  oscillator.start();
-  oscillator.stop(0.5);
+  playSound(keyCode, 'sine');
 };
 
 var badSound = function badSound(keyCode) {
+  playSound(keyCode, 'square');
+};
+
+var playSound = function playSound(keyCode, type) {
   var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
   var oscillator = audioCtx.createOscillator();
-  oscillator.type = 'square';
+  var gainNode = audioCtx.createGain();
+  oscillator.type = type;
   // keyCodes 66 -> 60 and hZ 200 -> 800
-  oscillator.frequency.value = 25 * keyCode - 1650 + 200;
-  oscillator.connect(audioCtx.destination);
+  var freq = 25 * keyCode - 1650 + 200;
+  oscillator.frequency.value = freq > 800 ? 800 : freq < 200 ? 200 : freq;
+
+  gainNode.connect(audioCtx.destination);
+  oscillator.connect(gainNode);
+
+  gainNode.gain.value = 0.1;
   oscillator.start();
   oscillator.stop(0.5);
+
+  console.log(audioCtx);
 };
--- src/sound.js
+++ src/sound.js
@@ -1,20 +1,24 @@
 const goodSound = function (keyCode) {
-  var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
-  var oscillator = audioCtx.createOscillator()
-  // keyCodes 66 -> 60 and hZ 200 -> 800
-  oscillator.frequency.value = ((25 * keyCode) - 1650) + 200
-  oscillator.connect(audioCtx.destination)
-  oscillator.start()
-  oscillator.stop(0.5)
+  playSound(keyCode, 'sine')
 }
 
 const badSound = function (keyCode) {
+  playSound(keyCode, 'square')
+}
+
+const playSound = function (keyCode, type) {
   var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
   var oscillator = audioCtx.createOscillator()
-  oscillator.type = 'square'
+  var gainNode = audioCtx.createGain()
+  oscillator.type = type
   // keyCodes 66 -> 60 and hZ 200 -> 800
-  oscillator.frequency.value = ((25 * keyCode) - 1650) + 200
-  oscillator.connect(audioCtx.destination)
+  var freq = ((25 * keyCode) - 1650) + 200
+  oscillator.frequency.value = freq > 800 ? 800 : freq < 200 ? 200 : freq
+
+  gainNode.connect(audioCtx.destination)
+  oscillator.connect(gainNode)
+
+  gainNode.gain.value = 0.1
   oscillator.start()
   oscillator.stop(0.5)
 }
