f523bd9593267b2c1c1dac46c7a51d9eef09d39d
Lucas Bersier <bers.lucas@gmail.com>
2018-08-28 20:46:09 -0400

    Change freqency equation, lower volume

--- src/sound.js
+++ src/sound.js
@@ -11,14 +11,14 @@ const playSound = function (keyCode, type) {
   var oscillator = audioCtx.createOscillator()
   var gainNode = audioCtx.createGain()
   oscillator.type = type
-  // keyCodes 66 -> 60 and hZ 200 -> 800
-  var freq = ((25 * keyCode) - 1650) + 200
-  oscillator.frequency.value = freq > 800 ? 800 : freq < 200 ? 200 : freq
+  // keyCodes 66 -> 60 and hZ 130 -> 1046
+  var freq = ((32 * keyCode) - 2080) + 130
+  oscillator.frequency.value = freq > 1046 ? 1046 : freq < 130 ? 130 : freq
 
   gainNode.connect(audioCtx.destination)
   oscillator.connect(gainNode)
 
-  gainNode.gain.value = 0.1
+  gainNode.gain.value = 0.009
   oscillator.start()
   oscillator.stop(0.5)
 }
